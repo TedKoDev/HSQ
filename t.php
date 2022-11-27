82a43fbce74c1b40ab7d81728ddfe9a01cc250d0
TED <ted.kordev@gmail.com>
2022-11-27 17:30:59 +0900

    findteacherProcess.php

--- t.php
+++ t.php
@@ -1,6 +1,5 @@
 <?php
-
-// == 내수업 목록 출력 프로세스==
+// == 강사 찾기 페이지  프로세스==  추후 출력항목 추가될 예정 
 //   #요구되는 파라미터 (fetch형태 json 구조로 전달) 
 
 //1. "token"    : "토큰값".
@@ -12,39 +11,111 @@
 // }
 
 // 코드 전개 구조
-// 1.토큰 수령후 User_Id 값 추출 
-// 2.User_Id 기준으로 DB User_Detail table내 해당 User_Id의 모든 칼럼이 채워져 있는지 를 체크한다. 
-// 3. U_D_Img, U_D_Bday, U_D_Sex, U_D_Contact, U_D_County, U_D_Residence, U_D_Language,U_D_Korean  칼럼중 default 값이 없을경우  yes  default값이 있는경우 no를 출력해서 프론트로 전달한다. 
+// 1. 토큰 수령후 User_Id 값 추출 (사용자)  
+// 2. 필요한 정보  (아래 참고사항 확인하기 )
+//  (강사의 User_Id, U_D_Img, U_Name, U_T_Special, U_D_Language,U_T_Intro, Price(Class_List, Class_List_Time_Price 정리)
+
+// 3. 정보 모아 프론트로 전달. 
+
+
+//     ## 참고사항  - 현재까지 진행상황 상 지금 처리해야 할 부분 :
+//     - 강사 id → 강사 클릭 했을 때 강사 상세 화면으로 이동하기 위해
+//     - 강사 프로필 이미지
+//     - 강사 이름
+//     - 강사 자격 (전문강사 or 튜터)
+//     - 강사의 구사 가능 언어 및 구사 수준
+//     - 강사 소개 (강사가 강사 등록할 때 작성한 강사 소개)
+//     - 수업료 (30분 기준 수업료. 만약 해당 강사가 등록한 수업이 여러개일 경우 그 중 가장 저렴한 수업료 보내기) -->
+
 
 
 
 include("./conn.php");
+// include("../jwt.php");
+
+// $jwt = new JWT();
+
+// // 토큰값, 항목,내용   전달 받음 
+// file_get_contents("php://input") . "<br/>";
+// $token      =   json_decode(file_get_contents("php://input"))->{"token"}; // 토큰 
+
+// //토큰 해체 
+// $data = $jwt->dehashing($token);
+// $parted = explode('.', base64_decode($token));
+// $payload = json_decode($parted[1], true);
+// // $User_ID =  base64_decode($payload['User_ID']);
+// $User_ID = '32';
+// $U_Name  = base64_decode($payload['U_Name']);
+// $U_Email = base64_decode($payload['U_Email']);
 
 
 
 //Class_List에 수업 목록확인  
-$sql = "SELECT * FROM User_Detail WHERE User_Id = '89'";
-$result = mysqli_query($conn, $sql);
-
-
-$row            = mysqli_fetch_array($result);
-$img           = $row['U_D_Img'];
-$bday             = $row['U_D_Bday'];
-$sex              = $row['U_D_Sex'];
-$country              = $row['U_D_County'];
-$residence        = $row['U_D_Residence'];
-$language          = $row['U_D_Language'];
-$korean                = $row['U_D_Korean'];
+$sql = "SELECT * FROM User_Teacher ";
+$response1 = mysqli_query($conn, $sql);
 
 
 
-if($img !='default'&&$bday !='default'&&$sex !='default'&&$country !='default'&&$residence !='default'&&$language !='default'&& $korean !='default'){
+$result1['data'] = array();
+while ($row1 = mysqli_fetch_array($response1)) {
+  $usid = $row1['1'];
 
-  echo 'yes'; 
-}else{
-  echo 'no';
-}
+  $send['User_Id'] = $row1['1'];
+  $send['U_T_Intro'] = $row1['2'];
+  $send['U_T_Special'] = $row1['4'];
 
 
+  //User_Detail 에서 이미지, 언어 수업 시간, 가격 확인   
+  $sql = "SELECT * FROM User_Detail WHERE User_Id = '$usid'";
+  $response2 = mysqli_query($conn, $sql);
+
+  $row2 = mysqli_fetch_array($response2);
+
+  $send['U_D_Img'] = $row2['2'];
+  $send['U_D_Language'] = $row2['8'];
+
+  //User 에서 유저 이름    
+  $sql = "SELECT * FROM User WHERE User_ID = '$usid'";
+  $response3 = mysqli_query($conn, $sql);
+
+  $row3 = mysqli_fetch_array($response3);
+
+  $send['U_Name'] = $row3['3'];
+
+
+  
+
+//Class_List와  class_list_Time_Price 와 join 을 통해서 userid가 만든  class list의 class id 값을  class_List_Time_Price와의 fk로 해서 리스트를 얻는다. 
+// 그중 가장 낮은 가격의 값을 얻는다. 
+
+
+
+
+//Class_List에 수업 목록확인  
+$sql = "SELECT * FROM Class_List WHERE User_Id = '{$usid}'";
+$response4 = mysqli_query($conn, $sql);
+
+$row4 = mysqli_fetch_array($response4);
+  $clid= $row4['0'];
+  $send['class_id']= $row4['0'];
+
+
+//Class_List_Time_Price 수업 시간, 가격 확인   
+$sql = "SELECT Class_List_Time_Price.CLass_Id, User_Id, Class_List_Time_Price.Time, Class_List_Time_Price.Price FROM HANGLE.Class_List Join Class_List_Time_Price 
+On Class_List.CLass_Id = Class_List_Time_Price.CLass_Id where Class_List.User_Id = '{$usid}' order by Class_List_Time_Price.Price asc limit 1";
+
+// $sql = "SELECT * FROM Class_List_Time_Price WHERE CLass_Id = '$clid'";
+$response5 = mysqli_query($conn, $sql);
+
+$row5 = mysqli_fetch_array($response5);
+   $send['Time'] = $row5['2'];
+   $send['Price'] = $row5['3']; 
+
+
+   array_push($result1['data'],$send);
+ 
+   }
+$result1["success"] = "1";
+echo json_encode($result1);
 
 mysqli_close($conn);
