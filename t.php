7c30bfcd258c611b5f7c129eed643462c9368fa2
TED <ted.kordev@gmail.com>
2022-11-30 15:34:54 +0900

    classdetailProcess, teacherdetailProcess

--- t.php
+++ t.php
@@ -1,105 +1,530 @@
 <?php
+// == 강사등록시 최초 화면출력 프로세스==  작업중 
+//   #요구되는 파라미터 (fetch형태도 요청 ) 
+//1. 토큰값  - token 
+//2. classid  - classid 
+
+// 보낼 줄 때 형태 
+// {
+//  "token"    : "토큰값".
+//  "classid"    : "classid".
+
+// }
+
+
+
+// #필요한 값 
+// - 해당수업 제목        Class_List.CL_Name
+// - 해당수업 설명        Class_List.CL_Disc
+// - 해당수업 학생수      Class_List.CL_People
+// - 해당수업 레벨        Class_List.CL_Level
+// - 해당수업 유형 (td)   Class_List.CL_Type
+// - 해당수업 가격        Class_List_Time_Price.Time, Class_List_Time_Price.Price
+
+// - 강사 이름                       User.U_Name 
+// - 강사 이미지                     User_Detail.U_D_Img
+// - 강사 자격 (튜터 or 전문)        User_Teacher.U_T_Special
+// - 강사 출신 국가      User_Detail.U_Country
+// - 강사 현재 거주 국가 User_Detail.U_Residence
+
+// - 강사의 수업가능시간 (이때 웹페이지 보는 유저의 시간대를 고려해서 보내줘야함) Teacher_Schedule.Schedule
+
+// - 해당 강사의 다른 수업 목록   
+
+
+
+// 돌아갈 값 
+// {
+//   "result": [
+//       {
+//           "CLass_Id": "34",
+//           "User_Id": "32",
+//           "CL_Name": "테스트",
+//           "CL_Disc": "테스트입니다",
+//           "CL_People": "1",
+//           "CL_Type": "회화 연습,문법",
+//           "CL_Level": "A1_C1",
+//           "tp": [
+//               {
+//                   "Time": "30",
+//                   "Price": "12"
+//               },
+//               {
+//                   "Time": "60",
+//                   "Price": "24"
+//               }
+//           ],
+//           "U_Name": "안해인22",
+//           "U_T_Special": "default",
+//           "U_D_Img": "1669356350PNG",
+//           "U_D_Country": "대한민국",
+//           "U_D_Residence": "일본",
+//           "Schedule": "99_100_145_146_194_196_243",
+//           "otherclasslist": [
+//               {
+//                   "class_id": "34",
+//                   "clname": "테스트",
+//                   "cldisc": "테스트입니다",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습,문법",
+//                   "cllevel": "A1_C1",
+//                   "cltp": null
+//               },
+//               {
+//                   "class_id": "39",
+//                   "clname": "한국어 수업1",
+//                   "cldisc": "수업1입니다",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습,발음,철자",
+//                   "cllevel": "A1_C1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "25"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "40",
+//                   "clname": "한국어 중급",
+//                   "cldisc": "중급 수업입니다",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습,문법,읽기,듣기",
+//                   "cllevel": "A1_C1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "10"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "18"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "153",
+//                   "clname": "wew",
+//                   "cldisc": "wew",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A1_C1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "33"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "33"
+//                       },
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "24"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "154",
+//                   "clname": "테스트ㅇㄴㅇㄴㅇ",
+//                   "cldisc": "테스트22",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A2_B2",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "10"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "20"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "155",
+//                   "clname": "44444",
+//                   "cldisc": "44444",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "444"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "444"
+//                       },
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "13"
+//                       },
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "13"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "156",
+//                   "clname": "555",
+//                   "cldisc": "555",
+//                   "clpeople": "1",
+//                   "cltype": "문법",
+//                   "cllevel": "",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "55"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "55"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "157",
+//                   "clname": "131313",
+//                   "cldisc": "131313",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A1_C2",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "13"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "158",
+//                   "clname": "55",
+//                   "cldisc": "55",
+//                   "clpeople": "1",
+//                   "cltype": "문법",
+//                   "cllevel": "",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "55"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "55"
+//                       },
+//                       {
+//                           "Time": "30",
+//                           "Price": "13"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "13"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "159",
+//                   "clname": "ㄱㄱ",
+//                   "cldisc": "ㄱㄱ",
+//                   "clpeople": "1",
+//                   "cltype": "발음",
+//                   "cllevel": "A1_C1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "111"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "111"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "160",
+//                   "clname": "666",
+//                   "cldisc": "666",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A1_C1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "666"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "666"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "161",
+//                   "clname": "17177",
+//                   "cldisc": "171717",
+//                   "clpeople": "1",
+//                   "cltype": "철자",
+//                   "cllevel": "C1_C2",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "12"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "14"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "162",
+//                   "clname": "테스트",
+//                   "cldisc": "테스트",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A1_C2",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "3000"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "6000"
+//                       }
+//                   ]
+//               },
+//               {
+//                   "class_id": "163",
+//                   "clname": "테스트22",
+//                   "cldisc": "테스ㅡ22",
+//                   "clpeople": "1",
+//                   "cltype": "회화 연습",
+//                   "cllevel": "A1_B1",
+//                   "cltp": [
+//                       {
+//                           "Time": "30",
+//                           "Price": "30000"
+//                       },
+//                       {
+//                           "Time": "60",
+//                           "Price": "60000"
+//                       }
+//                   ]
+//               }
+//           ]
+//       }
+//   ],
+//   "success": "1"
+// }
 
 
 include("./conn.php");
 include("./jwt.php");
 
-
-
-
-
-
-
 $jwt = new JWT();
 
-// 토큰값, 항목,내용   전달 받음 
+// 토큰값, 이미지  전달 받음 
 file_get_contents("php://input") . "<br/>";
-$token      =   json_decode(file_get_contents("php://input"))->{"token"}; // 토큰 
-$plus      =   json_decode(file_get_contents("php://input"))->{"plus"}; // 토큰 
+$token = json_decode(file_get_contents("php://input"))->{"token"}; // 토큰 
+// $classid = json_decode(file_get_contents("php://input"))->{"classid"}; // 수업id 
+
 
 
 //토큰 해체 
 $data = $jwt->dehashing($token);
 $parted = explode('.', base64_decode($token));
 $payload = json_decode($parted[1], true);
+// $User_ID =  base64_decode($payload['User_ID']);
 $User_ID = 32;
 $U_Name  = base64_decode($payload['U_Name']);
 $U_Email = base64_decode($payload['U_Email']);
 
+$classid =  34;
 
-$i= 0 ;
+//현재 로그인한 유저의 U_D_Timeze 값을 가져옴   
+$sql = "SELECT U_D_Timezone FROM User_Detail WHERE User_Id = '{$User_ID}'";
+$result = mysqli_query($conn, $sql);
+$row1 = mysqli_fetch_array($result);
+$timezone = $row1['0'];
+$send['CONNECT_USER_TIMEZONE'] = $row1['0'];
 
-$start =  $i + (20* $plus);
-$till = 20;
 
-
+//배열생성 
 $result1['result'] = array();
 $result2['timeprice'] = array();
 
-    //Class_List와  class_list_Time_Price 와 join 을 통해서 userid가 만든  class list의 class id 값을  class_List_Time_Price와의 fk로 해서 리스트를 얻는다. 
-    // 그중 가장 낮은 가격의 값을 얻는다. 
 
-    //Class_List에 수업 목록확인  
-    $sql = "SELECT * FROM Class_List order by  Class_Id DESC LIMIT $start, $till";
-    $response1 = mysqli_query($conn, $sql);
+//현재 로그인한 유저의 U_D_Timeze 값을 가져옴   
+$sql = "SELECT * FROM Class_List WHERE Class_Id = '{$classid}'";
+$response1 = mysqli_query($conn, $sql);
 
-    while ($row1 = mysqli_fetch_array($response1)) {
-      $clid = $row1['0'];
-      $usid = $row1['1'];
-    
-      $send1['class_id'] = $row1['0'];
-      // $send1['user_id'] = $row1['1'];
-      $send1['clname'] = $row1['2'];
-      $send1['cldisc'] = $row1['3'];
-      $send1['clpeople'] = $row1['4'];
-      $send1['cltype'] = $row1['5'];
-      $send1['cllevel'] = $row1['6'];
-    
-      $sql = "SELECT U_Name FROM User where User_Id = $usid ";
-      $response2 = mysqli_query($conn, $sql);
-      $send1['name'] = $row1['0'];
-    
-      
-      $sql = "SELECT U_T_Special FROM User_Teacher where User_Id = $usid ";
-      $response2 = mysqli_query($conn, $sql);
-      $send1['special'] = $row1['0'];
-    
-    
-    
-    
-      //Class_List_Time_Price 수업 시간, 가격 확인   
-      $sql = "SELECT * FROM Class_List_Time_Price WHERE CLass_Id = '$clid'";
-      $response3 = mysqli_query($conn, $sql);
-    
-      while ($row2 = mysqli_fetch_array($response3)) {
-    
-        $tp['Time'] = $row2['2'];
-        $tp['Price'] = $row2['3'];
-    
-        array_push($result2['timeprice'], $tp);
-      }
-      //  echo json_encode($result2);
-    
-      $send1['tp'] = $result2['timeprice'];
-    
-      array_push($result1['result'], $send1);
-      $result2['timeprice'] = array();
-    }
-    // $result1["success"] = "1";
-
-    
+$row1 = mysqli_fetch_array($response1);
 
 
-    if ($response2) { //정상적으로 저장되었을때 
+$clid = $row1['0'];
+$usid = $row1['1'];
+$send['CLass_Id'] = $row1['0'];
+$send['User_Id'] = $row1['1'];
+$send['CL_Name'] = $row1['2'];
+$send['CL_Disc'] = $row1['3'];
+$send['CL_People'] = $row1['4'];
+$send['CL_Type'] = $row1['5'];
+$send['CL_Level'] = $row1['6'];
 
-      $result1["success"] = "yes";
-      echo json_encode($result1);
-      mysqli_close($conn);
-    } else {
-    
-      $result1["success"]   =  "no";
-      // echo json_encode($result1);
-      mysqli_close($conn);
-    }
 
-    
+  //Class_List_Time_Price 수업 시간, 가격 확인   
+  $sql = "SELECT * FROM Class_List_Time_Price WHERE CLass_Id = '$clid'";
+  $response2 = mysqli_query($conn, $sql);
+
+  while ($row2 = mysqli_fetch_array($response2)) {
+
+    $tp['Time'] = $row2['2'];
+    $tp['Price'] = $row2['3'];
+
+    array_push($result2['timeprice'], $tp);
+  }
+  $send['tp'] = $result2['timeprice'];
+
+
+
+
+//Class_List에 수업 목록확인  
+$sql = "SELECT 
+User.U_Name, 
+User_Teacher.U_T_Special,  
+User_Detail.U_D_Img,
+User_Detail.U_D_Country,
+User_Detail.U_D_Residence,
+Teacher_Schedule.Schedule
+FROM User
+JOIN User_Detail
+  ON User.User_ID = User_Detail.User_Id
+JOIN User_Teacher
+  ON User_Teacher.User_Id = User_Detail.User_Id 
+JOIN Teacher_Schedule
+  ON Teacher_Schedule.User_Id = User_Detail.User_Id
+ where User.User_Id = '$usid' ";
+$response3 = mysqli_query($conn, $sql);
+
+$row1 = mysqli_fetch_array($response3);
+
+
+$send['U_Name'] = $row1['0'];
+$send['U_T_Special'] = $row1['1'];
+$send['U_D_Img'] = $row1['2'];
+$send['U_D_Country'] = $row1['3'];
+$send['U_D_Residence'] = $row1['4'];
+
+$plan1 = $row1['5'];
+
+
+//db에서 가져온 시간별 칸 값을 _ 기호를 기준으로 분리한다. 
+$planresult = (explode("_", $plan1));
+
+// echo $planresult;
+
+$resultarray = array();
+//
+
+foreach ($planresult as $val) {
+
+  $val . "</br>";
+
+  $save = $val - $timezone * 2;
+
+  // echo $save;
+  if ($save > 336) {
+
+    $save = $save - 336;
+  }
+
+
+  array_push($resultarray, $save);
+}
+
+$string = implode("_", $resultarray);
+$send['Schedule'] = $string;
+
+//강사 다른 수업 관련 배열 
+// $result3['class'] = array();
+// $result4['cltimeprice'] = array();
+
+// $result1["success"] = "1";
+$result1["success"] = "1";
+
+
+
+
+
+
+
+
+//Class_List에 수업 목록확인  
+$sql = "SELECT * FROM Class_List WHERE User_Id = '{$usid}'";
+$response1 = mysqli_query($conn, $sql);
+
+
+//배열생성 
+$result3['clist'] = array();
+$result4['cltimepirce'] = array();
+
+while ($row1 = mysqli_fetch_array($response1)) {
+  $clid = $row1['0'];
+
+  $send1['class_id'] = $row1['0'];
+  $send1['clname'] = $row1['2'];
+  $send1['cldisc'] = $row1['3'];
+  $send1['clpeople'] = $row1['4'];
+  $send1['cltype'] = $row1['5'];
+  $send1['cllevel'] = $row1['6'];
+
+
+  //Class_List_Time_Price 수업 시간, 가격 확인   
+  $sql = "SELECT * FROM Class_List_Time_Price WHERE CLass_Id = '$clid'";
+  $response2 = mysqli_query($conn, $sql);
+
+  while ($row2 = mysqli_fetch_array($response2)) {
+
+    $tp['Time'] = $row2['2'];
+    $tp['Price'] = $row2['3'];
+
+    array_push($result4['cltimeprice'], $tp);
+  }
+  //  echo json_encode($result2);
+
+  $send1['cltp'] = $result4['cltimeprice'];
+
+  array_push($result3['clist'], $send1);
+  $result4['cltimeprice'] = array();
+}
+$send['otherclasslist'] = $result3["clist"];
+
+array_push($result1['result'], $send);
+echo json_encode($result1);
+mysqli_close($conn);
+
