3fa19302cb64efd3071587f39a19b73593cd4658
TED <ted.kordev@gmail.com>
2022-12-14 09:20:41 +0900

    reservfinalProcess.php 이메일 전송확인

--- reservefinal/reservefinalProcess.php
+++ reservefinal/reservefinalProcess.php
@@ -34,11 +34,19 @@ $jwt = new JWT();
 file_get_contents("php://input") . "<br/>";
 
 $token      =   json_decode(file_get_contents("php://input"))->{"token"}; // 사용자(학생)토큰 
-$classid    =   json_decode(file_get_contents("php://input"))->{"classid"}; // 수업id 
+$class_id    =   json_decode(file_get_contents("php://input"))->{"class_id"}; // 수업id 
+// $class_id    =   167; // 수업id 
 $tp         =   json_decode(file_get_contents("php://input"))->{"tp"}; // 수업시간 
-$plan       =   json_decode(file_get_contents("php://input"))->{"plan"}; // 수업일정 
-$cmethod    =   json_decode(file_get_contents("php://input"))->{"cmethod"}; // 수업방식 
-$memo       =   json_decode(file_get_contents("php://input"))->{"memo"}; // 강사메모
+// $tp         =  30 ; // 수업시간 
+$schedule       =   json_decode(file_get_contents("php://input"))->{"schedule"}; // 수업일정 
+// $schedule       =   '1670385600000_1670383800000_1670387400000_1670389200000_1670391000000'; // 수업일정 
+
+
+$class_method    =   json_decode(file_get_contents("php://input"))->{"class_register_method"}; // 수업방식 
+// $class_method    =   0; // 수업방식 
+
+$class_register_memo       =   json_decode(file_get_contents("php://input"))->{"class_register_memo"}; // 강사메모
+// $class_register_memo       =  '밥바오밥'; // 강사메모
 
 
 //사용자(학생)토큰 해체 
@@ -46,6 +54,7 @@ $data = $jwt->dehashing($token);
 $parted = explode('.', base64_decode($token));
 $payload = json_decode($parted[1], true);
 $User_ID = base64_decode($payload['User_ID']); //학생의 userid
+$User_ID = 320; //학생의 userid
 $U_Name  = base64_decode($payload['U_Name']);  //학생의 이름
 $U_Email = base64_decode($payload['U_Email']); //학생의 Email
 $timezone = base64_decode($payload['TimeZone']); //사용자(학생)의 TimeZone
@@ -63,11 +72,11 @@ $timezone = base64_decode($payload['TimeZone']); //사용자(학생)의 TimeZone
 
 
 //수업id 기준으로 해당 수업의 정보 
-$sql = "SELECT * FROM Class_List WHERE CLass_Id = '{$classid}'";
+$sql = "SELECT * FROM Class_List WHERE class_id = '{$class_id}'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
 
-$tusid = $row['1'];
+ $tusid = $row['1'];
 $clname = $row['2'];
 $cldisc = $row['3'];
 $cltype = $row['4'];
@@ -76,69 +85,63 @@ $cllevel = $row['5'];
 
 
 
-
-
-
 //1시간 = 3600000;
 $hour   = 3600000;
 
 // 프론트단에서 전달받은 시간별 칸 값을 _ 기호를 기준으로 분리한다. 
-$explan = (explode("_", $plan));
-$tzplan = array(); //Timezone 적용을 위한 배열 생성
-foreach($explan as $val){ 
+$exschedule = (explode("_", $schedule));
+
+$tzschedule = array(); //Timezone 적용을 위한 배열 생성
+foreach($exschedule as $val){ 
 $save = $val - $timezone* $hour;
-$date = date('Y-m-d H:i:s',  $save);
-
-
+// $date = date('Y-m-d H:i:s',  $save);
 
 // Class_Add DB TABLE에 저장 
-$sqlClassAdd = "INSERT INTO Class_Add (User_Id_s, User_Id_t, CLass_Id, CTime, C_A_Schedule,C_A_Memo,C_A_Status,C_A_Method, C_A_AnswerDate, C_A_Date) 
-           VALUES ('$User_ID', '$tusid','$classid', '$tp', '$tzplanresult', '$memo', '0', '$cmethod', '0' , now())";
+$sqlClassAdd = "INSERT INTO Class_Add (user_id_student, user_id_teacher, class_id, class_time, schedule_list,class_register_memo,class_register_status,class_register_method, class_register_answer_date, class_register_date) 
+           VALUES ('$User_ID', '$tusid','$class_id', '$tp', '$save', '$class_register_memo', '0', '$class_method', '0' , now())";
 $insert = mysqli_query($conn, $sqlClassAdd);
 
 if ($sqlClassAdd) 
 $last_CAID = mysqli_insert_id($conn); // 마지막으로 insert 된 값의 idx 값 가져오기 용도: 메일 전송후 수업 예약 승낙 여부를 Class_ADD tb내 C_A_Status 상태 변경용. 
 
 
-
-
-
-array_push($tzplan,$save);
+array_push($tzschedule,$save);
 }
-$tzplanresult = implode("_",$tzplan); // 다시 합체 
-
-
-
+$tzscheduleresult = implode("_",$tzschedule); // 다시 합체 
 
 
 
 
 // 위의 수업을 개설한 강사의 '이름','email', 'timezone' 정보 
-$sql = "SELECT User.U_Name, User.U_Email, User_Detail.U_D_Timezone
+$sql = "SELECT User.user_name, User.user_email, User_Detail.user_timezone
 FROM User
-JOIN User_Detail ON User.User_ID = User_Detail.User_Id
- where User.User_Id = '$tusid' ";
+JOIN User_Detail ON User.user_id = User_Detail.user_id
+ where User.user_id = '$tusid' ";
 $result = mysqli_query($conn, $sql);
 $row1 = mysqli_fetch_array($result);
 
-$tname = $row1['0'];
-$temail = $row1['1'];
-$tTimezone = $row1['2'];
+ $tname = $row1['0'];
+ $temail = $row1['1'];
+ $tTimezone = $row1['2'];
 
 
 
 
-// 신청한 수업 일정(plan) 값을 강사에게 보내주기 위한 코드 이며 강사의 TIMEZONE 기준으로 변환 되어 $sendtimeresult 에 저장됨 . 
+// 신청한 수업 일정(schedule) 값을 강사에게 보내주기 위한 코드 이며 강사의 TIMEZONE 기준으로 변환 되어 $sendtimeresult 에 저장됨 . 
 $sendtime = array();
-foreach($explan as $val){
-  $i =0;
-$save = $val + $tTimezone* $hour/1000;
-$date = date('Y-m-d H:i:s',  $save);
+$i =0;
+foreach($exschedule as $val){
+
+echo'dd'. $save1 = ($val + $tTimezone * $hour)/1000 . '</br>';
+// echo $save2 = $save1/1000;
+
+echo $date = date('Y-m-d H:i:s', $save1);
 
 $i =$i +1;
+echo $i;
 array_push($sendtime,$date);
 }
-$sendtimeresult = implode(",",$sendtime);
+$sendtimeresult = implode(",  ",$sendtime);
 
 
 //메일 전송 
@@ -154,17 +157,10 @@ Class-Name: '.$clname.''. "<br/>".'
 Class-Type: '.$cltype.''. "<br/>".'
 Class-Level: '.$cllevel.''. "<br/>".'
 Class-TIME: '.$tp.' min'. "<br/>".'
-Schedule:'.$i.'time(s)'. "<br/>".''.$sendtimeresult.''. "<br/>".'
-Message: '.$memo."<br/>".'
+Schedule:  '.$i.'time(s)'. "<br/>".''.$sendtimeresult.''. "<br/>".'
+Message: '.$class_register_memo."<br/>".'
 ------------------------'. "<br/>".'
  
-AGREE(예약승인) : Please click this link to accept reservation for class :'.$clname.''. "<br/>".'
-http://localhost/reservefinal/reserveApproval.php?email='.$U_Email.'&CAID='.$last_CAID.'&agree='.'1'.'
-'. "<br/>".'
-'. "<br/>".'
-
-REFUSE(예약거절): Please click this link to accept reservation for class :'.$clname.''. "<br/>".'
-http://localhost/reservefinal/reserveApproval.php?email='.$U_Email.'&CAID='.$last_CAID.'&agree='.'2'.'
 
 
 '; // Our message above including the link;
@@ -208,3 +204,12 @@ mysqli_close($conn);
 
  ;
 }
+
+
+// AGREE(예약승인) : Please click this link to accept reservation for class :'.$clname.''. "<br/>".'
+// http://localhost/reservefinal/reserveApproval.php?email='.$U_Email.'&CAID='.$last_CAID.'&agree='.'1'.'
+// '. "<br/>".'
+// '. "<br/>".'
+
+// REFUSE(예약거절): Please click this link to accept reservation for class :'.$clname.''. "<br/>".'
+// http://localhost/reservefinal/reserveApproval.php?email='.$U_Email.'&CAID='.$last_CAID.'&agree='.'2'.'
--- t1.php
+++ t1.php
@@ -1,182 +1,26 @@
 <?php
 
+ echo $tt = microtime().'</br>';
+ echo $tt = 1670385556,800 .'</br>';
+$date = date('Y-m-d H:i:s',  $tt);
+echo $tt.'</br>'; 
+echo "The date is $date.".'</br>'.'</br>'.'</br>';  
 
-include("./conn.php");
-include("./jwt.php");
-
-
-
-$jwt = new JWT();
-
-// 토큰값, 항목,내용   전달 받음 
-file_get_contents("php://input") . "<br/>";
-$token      =   json_decode(file_get_contents("php://input"))->{"token"}; // 사용자(학생)토큰 
-
-//사용자(학생)토큰 해체 
-$data = $jwt->dehashing($token);
-$parted = explode('.', base64_decode($token));
-$payload = json_decode($parted[1], true);
-$User_ID = base64_decode($payload['User_ID']); //학생의 userid
-// $User_ID = 32; //학생의 userid
-$U_Name  = base64_decode($payload['U_Name']);  //학생의 이름
-$U_Email = base64_decode($payload['U_Email']); //학생의 Email
-$timezone = base64_decode($payload['TimeZone']); //사용자(학생)의 TimeZone
+// $timestamp = strtotime("2022-12-01 11:30").'</br>';
+// echo $timestamp.'</br>';
+// $date = date('Y-m-d H:i:s',  $timestamp);
+// echo $timestamp.'</br>'; 
+// echo "The date is $date".'</br>'.'</br>'; 
 
 
 
-# 필요값 
-// 어떤 내용이 필요한지를 표시 ( clist-수업목록, cdetail-수업상세, tclist-강사의 수업목록)
-$kind          =   json_decode(file_get_contents("php://input"))->{"kind"}; // 강사의 User_id 
-$clReserveCheck     =   json_decode(file_get_contents("php://input"))->{"clReserveCheck"}; // 예약된 수업 리스트 / 상세 
-
-// $kind            =   'clist';         //  
-// $kind            =   'cdetail';       //  
-// $clReserveCheck  =   'cdetail';       // 예약된 수업 상세 페이지 
-
-// $kind          =   'tcdetail';      // 
-$kind          =   'tclist';      // 
-// $clReserveCheck  =  'all';          // 
-// $clReserveCheck  =  'approved';     // 
-// $clReserveCheck  =  'not_approved'; // 
-// $clReserveCheck  =  'done';         // 
-// $clReserveCheck  =  'reply';        // 
+// echo $nomal = strtotime("2022-12-01 11:30")."<br/>";
+// echo "2022-12-01 11:30 : ".strtotime("2022-12-01 11:30")."<br/>";
+// echo  $plusone = strtotime("2022-12-01 11:30 +1 hours")."<br/>";
+// echo "2022-12-01 11:30  기준으로 1시간 뒤 : ".strtotime("2022-12-01 11:30 +1 hours")."<br/>";
 
 
-// $kind          =   'cdetail'; // 강사의 User_id 
-// 수업목록        : kind = clist 
-// 수업상세        : kind = cdetail    
-// 예약체크        : kind = clReserveCheck    // 전체 목록 또는 상세 정보 
-// 예약체크 필요값 : value = all, approved, not_approved  ,done ,reply
-// value : 
-// 1. all (해당 유저가 예약/완료한 모든 수업)
-// 2. approved (예약 승인 되었지만 아직 수강은 안한 수업)
-// 3. not_approved (예약 신청은 했는데 강사가 아직 예약 승인은 안한 수업)
-// 4. done (완료한 수업) 
+//  $qq=$plusone-$nomal;
+// echo "값 차이 1시간 : ". $qq;
 
-
-
-
-
-// 필요한 class의 id 값이 필요함 
-$classid       =   json_decode(file_get_contents("php://input"))->{"classid"}; // 수업번호 
-$classaddid       =   json_decode(file_get_contents("php://input"))->{"classaddid"}; // 예약한 수업 번호 
-
-
-// $classaddid       =  26; // 예약한 수업 번호 
-// $classid       =   34; // 수업번호 
-// 강사의 수업목록 : kind = tclist
-// 강사의 userid 값이 필요함 
-// $tusid       =   json_decode(file_get_contents("php://input"))->{"tusid"}; // 강사의 User_id 
-$tusid       =  320; // 강사의 User_id 
-
-
-//====================================================================================================
-
-//수업목록, 강사의 수업목록 이 필요할 경우 아래의 항목에 (아무런) 값을 넣어 보내줘야 출력됨.  
-
-
-$clname     =   json_decode(file_get_contents("php://input"))->{"clname"};   // 수업이름 
-$cldisc     =   json_decode(file_get_contents("php://input"))->{"cldisc"};   // 수업설명 
-$clpeople   =   json_decode(file_get_contents("php://input"))->{"clpeople"}; // 수업인원 
-$cltype     =   json_decode(file_get_contents("php://input"))->{"cltype"};   // 수업유형 
-$cllevel    =   json_decode(file_get_contents("php://input"))->{"cllevel"};  // 수업레벨 
-$cltime     =   json_decode(file_get_contents("php://input"))->{"cltime"};   // 수업시간
-$clprice    =   json_decode(file_get_contents("php://input"))->{"clprice"};  // 수업가격
-$timg       =   json_decode(file_get_contents("php://input"))->{"timg"};     // 강사이미지
-$tname      =   json_decode(file_get_contents("php://input"))->{"tname"};    // 강사이름
-
-$clname     =  1; // 수업이름 
-$cldisc     =  1; // 수업설명 
-$clpeople   =  1; // 수업인원 
-$cltype     =  1; // 수업유형 
-$cllevel    =  1; // 수업레벨 
-$cltime     =  1; // 수업시간
-$clprice    =  1; // 수업가격
-$timg       =  1; // 강사이미지
-$tname      =  1; // 강사이름
-
-
-
-// 더보기 (페이징)처리 용 
-$plus       =   json_decode(file_get_contents("php://input"))->{"plus"};     // 더보기 
-
-
-
- if ($kind == 'tclist') {
-  // 필요한 값이 특정 강사의 수업 목록 이면 
-
-  $result1['data'] = array();
-  $result2['timeprice'] = array();
-  $result3['result'] = array();
-
-
-  $list = array();
-  array_push($list, 'CLass_Id');
-  if ($clname != null) {
-    array_push($list, 'CL_Name');
-  }
-  if ($cldisc != null) {
-    array_push($list, 'CL_Disc');
-  }
-  if ($clpeople != null) {
-    array_push($list, 'CL_People');
-  }
-  if ($cltype != null) {
-    array_push($list, 'CL_Type');
-  }
-  if ($cllevel != null) {
-    array_push($list, 'CL_Level');
-  }
-
-  $string = implode(",", $list);
-
-
- 
-  //Class_List에 수업 목록확인  
-  $sql = "SELECT  $string FROM Class_List WHERE User_Id_t = '{$tusid}'";
-  $response1 = mysqli_query($conn, $sql);
-
-  foreach ($response1 as $key) {
-
-
-    // echo key($key); // 키
-    // echo current($key); // 값
-    $clid = current($key); // 값
-
-    array_push($result1['data'], $key);
-
-
-    if ($clprice != null) {
-      //Class_List_Time_Price 수업 시간, 가격 확인   
-      $sql = "SELECT * FROM Class_List_Time_Price WHERE CLass_Id = '$clid'";
-      $response2 = mysqli_query($conn, $sql);
-      
-      while ($row2 = mysqli_fetch_array($response2)) {
-
-        $tp = $row2['3'];
-
-        array_push($result2['timeprice'], $tp);
-      }
-
-
-      $send1['tp'] = $result2['timeprice'];
-    }
-
-
-
-    array_push($result1['data'], $send1);
-    $result2['timeprice'] = array();
-  }
-  $send['class'] = $result1['data'];
-  array_push($result3['result'], $send);
-  $result3["success"] = "1";
-  echo json_encode($result3);
-  mysqli_close($conn);
-
-    // $result1['data'] = array();
-    // $result2['timeprice'] = array();
-    // $result3['result'] = array();
-  
-} else if ($kind == 'tcdetail'){
-
-}
+?>
