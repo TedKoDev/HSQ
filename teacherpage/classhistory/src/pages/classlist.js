a3c2496082d25a88b6377a492954c12e99291011
ahnhaein <ahnhaein626@gmail.com>
2022-12-18 11:00:09 +0900

    ss

--- teacherpage/classhistory/src/pages/classlist.js
+++ teacherpage/classhistory/src/pages/classlist.js
@@ -138,7 +138,7 @@ const statusChange = (status, $classStyle) => {
         status = "취소됨"
     } else if (status == "3") {
         status = "완료됨"
-        $classStyle.setAttribute("class", "text-sm text-gray-400");
+        // $classStyle.setAttribute("class", "text-sm text-gray-400");
     }
     return status;
 }
