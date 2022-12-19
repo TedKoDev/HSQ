999d2ca503cf71da58f42f64fd68744735314bcb
TED <ted.kordev@gmail.com>
2022-12-19 15:59:14 +0900

    manageupdateProcess.php

--- manageschedule/manageupdateProcess.php
+++ manageschedule/manageupdateProcess.php
@@ -39,6 +39,11 @@ $token     =   json_decode(file_get_contents("php://input"))->{"token"}; // 토�
 $repeat      =   json_decode(file_get_contents("php://input"))->{"repeat"}; // 몇주 반복 여부  4, 8, 12 주  
 $utc      =   json_decode(file_get_contents("php://input"))->{"user_timezone"}; // 
 $plan      =   json_decode(file_get_contents("php://input"))->{"schedule_list"};  // 일정 
+
+
+
+
+
 // $plan      =  '1669894200_1669896000_1669897800';
 // $plan      =  '1000_2000';
 // $repeat    = 8; // 몇주 반복 여부  4, 8, 12 주  
@@ -49,7 +54,6 @@ $hour = 3600000;
 
 
 
-// error_log("$time_now, $position, $desc\n", "3", "/php.log");
 
 //토큰 해체 
 $data = $jwt->dehashing($token);
@@ -58,7 +62,7 @@ $payload = json_decode($parted[1], true);
 $User_ID =  base64_decode($payload['User_ID']);
 $U_Name  = base64_decode($payload['U_Name']);
 $U_Email = base64_decode($payload['U_Email']);
-
+$timezone1 = base64_decode($payload['TimeZone']); //사용자(학생)의 TimeZone
 
 
 //U_D_Timeze 값을 가져옴   
@@ -67,12 +71,13 @@ $response1 = mysqli_query($conn, $sql);
 $row1 = mysqli_fetch_array($response1); 
 $timezone = $row1['0'].'</br>';
 
+error_log("$plan , $utc,$timezone1,$timezone \n", "3", "../php.log");
 
 
 
   
-$check = "SELECT * FROM Teacher_Schedule where user_id_teacher = '$User_ID'";
-$checkresult = mysqli_query($conn, $check);
+// $check = "SELECT * FROM Teacher_Schedule where user_id_teacher = '$User_ID'";
+// $checkresult = mysqli_query($conn, $check);
 
 
 // 프론트단에서 전달받은 시간별 칸 값을 _ 기호를 기준으로 분리한다. 
@@ -96,7 +101,7 @@ json_encode($resultarray);
 
 
     //  $result = "DELETE FROM Teacher_Schedule   WHERE User_Id_s = '32' ";
-     $result = "DELETE FROM Teacher_Schedule   WHERE user_id_teacher = '$User_ID' ";
+     $result = "DELETE FROM Teacher_Schedule   WHERE user_id_teacher = '$User_ID' and teacher_schedule_status = '9'";
      $response = mysqli_query($conn, $result);
 
  foreach($resultarray as $val){
