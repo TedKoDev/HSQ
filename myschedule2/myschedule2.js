a5d36a1c5fef8aed6e94ab29832da25f39a26570
ahnhaein <ahnhaein626@gmail.com>
2022-12-21 20:27:32 +0900

    mvc 패턴 어거지로 안하기로 결정

--- myschedule2/myschedule2.js
+++ myschedule2/myschedule2.js
@@ -1,12 +1,13 @@
-import {model} from './src/model.js';
-import {view} from './src/view.js';
-import {controller} from './src/controller.js';
+// import {model} from './src/m_myschedule.js';
+// import {view} from './src/v_myschedule.js';
+// import {controller} from './src/c_myschedule.js';
 
 
-window.addEventListener('DOMContentLoaded', function() {
+// window.addEventListener('DOMContentLoaded', function() {
    
-    // model, view, controller 선언
-    const schedule_modal = model();
-    const schedule_view = view(schedule_modal);
-    const schedule_controller = controller(schedule_modal, schedule_view);
-});
+//     // model, view, controller 선언
+//     const schedule_modal = model();
+//     const schedule_view = view(schedule_modal);
+//     const schedule_controller = controller(schedule_modal, schedule_view);
+// });
+
---
+++ myschedule2/src/c_myschedule.js
@@ -0,0 +1,127 @@
+import { $, $_all } from "/utils/querySelector.js";
+
+
+//controller
+export async function controller(model, view){
+        
+    // const addItem = function(e) {
+
+    //     console.log("addItem");
+
+    //     e.preventDefault();
+
+    //     var add_value = add_input.value;
+
+    //     Data.addItem(add_value);
+
+    //     UI.showList(Data.list);
+
+    //     form.reset();
+    // };
+    
+    // const searchItem = function() {
+
+    //     console.log("searchItem");
+        
+
+    //     const search_value = search_input.value;
+
+    //     const filter_list = Data.list.filter(function(item) {
+    //         return item.content.indexOf(search_value) > -1;
+    //     }); 
+        
+    //     UI.showList(filter_list);
+    // }
+
+    // const removeItem = function(e) {
+
+    //     console.log("removeItem");
+
+    //     if(e.target.tagName !== 'I') return;
+
+    //     var item_id = e.target.parentNode.parentNode.id;
+    //     var item_index = item_id.split('-')[1];
+
+    //     Data.removeItem(item_index);
+
+    //     UI.showList(Data.list);
+    // }
+
+    // const checkItem = function(e) {
+
+    //     console.log("checkItem");
+        
+    //     if(e.target.tagName !== 'INPUT') return;
+
+    //     const item_id = e.target.parentNode.parentNode.id;
+    //     const item_index = item_id.split('-')[1];
+        
+    //     Data.checkItem(item_index);
+
+    //     UI.showList(Data.list);
+    // }
+
+    // 초기 세팅 (유저 id, 타임존, 수업 목록 가져오기)
+    const init = async function() {
+        
+        console.log(model);
+        // model.get_utc();
+        // model.getSchedulesList();
+    }
+
+    // 이전, 다음 버튼 클릭 시 캘린더 재 렌더링 되는 함수
+    const reRender = function(e) {
+
+        const target = e.target.id;        
+
+        if (target == "prev_btn") {
+
+            console.log("이전");
+        }
+        else {
+            console.log("다음");
+        }
+    }
+
+
+    // 초기세팅
+    init();
+
+    // 이전, 다음 버튼 선언
+    const prev_btn = $('.prev_btn');
+    const next_btn = $('.next_btn');
+
+    // 수업 버튼 선언
+    const class_btn = $_all('.schedule_list');
+    
+    // 이전, 다음 버튼 이벤트 리스너 선언
+    prev_btn.addEventListener('click', reRender);
+    next_btn.addEventListener('click', reRender);
+
+    // 수업 버튼 이벤트 리스너 선언
+    class_btn.forEach( (btn) => {
+        btn.addEventListener('click', (e) =>{
+
+            // 스케줄 블록안에 있는 버튼 중 클릭한 위치에서 가장 가까운 버튼 선택
+            const target = e.target.closest("button");
+            if (!(target instanceof HTMLButtonElement)) return;
+
+        });
+    })
+
+    
+
+    // const form = document.forms['list-form'];
+    // const add_input = form['add-item__input'];
+    // const search_input = form['search-item__input'];
+    
+    // const section = document.querySelector('section');
+    
+    // form.addEventListener('submit', addItem);
+    
+    // search_input.addEventListener('input', searchItem);
+
+    // section.addEventListener('click', removeItem);
+    // section.addEventListener('change', checkItem);
+};
+
