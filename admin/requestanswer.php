a0344ccd30962f24c6ad4952424a42b7291a982b
TED <ted.kordev@gmail.com>
2022-12-29 11:31:52 +0900

    f

--- admin/requestanswer.php
+++ admin/requestanswer.php
@@ -14,7 +14,7 @@ classreservProcess.php
 4. 수업일정
 5. 수업방식(한글스퀘어 = 0, zoom = 1 )
 6. 강사메모
-*/
+ */
 
 include("../conn.php");
 include("../jwt.php");
@@ -31,16 +31,12 @@ use PHPMailer\PHPMailer\SMTP;
 
 $jwt = new JWT();
 // 토큰값, 항목,내용   전달 받음 
-file_get_contents("php://input") ;
+file_get_contents("php://input");
 
 $token              =   json_decode(file_get_contents("php://input"))->{"token"}; // 사용자토큰 
 $request_user_id    =   json_decode(file_get_contents("php://input"))->{"request_user_id"}; // 강사신청자id 
 $answer_text            =   json_decode(file_get_contents("php://input"))->{"answer_text"}; // 답변 작성 내용 
 $answer            =   json_decode(file_get_contents("php://input"))->{"answer"}; // 전문강사, 일반강사, 거절  내용  
-
-
-
-
 
 
 //사용자(학생)토큰 해체 
@@ -55,214 +51,95 @@ $timezone = base64_decode($payload['TimeZone']); //사용자(학생)의 TimeZone
 
 
 
-// error_log("token : $token , user_id : $User_ID ,  class_id :  $class_id , 수업시간(30,60) :  $tp  , 수업일정: $schedule,  수업도구: $class_method, 수업메모:  $class_register_memo   \n", "3", "../php.log");
 
 
 
 
+if ($answer == 1) {
+  $answer_result = '전문강사/Special_Tutor';
 
-//수업id 기준으로 해당 수업의 정보 
-$sql = "SELECT * FROM Class_List WHERE class_id = '{$class_id}'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result);
-
- $tusid = $row['1'];
-$clname = $row['2'];
-$cldisc = $row['3'];
-$cltype = $row['4'];
-$cllevel = $row['5'];
+  $select = "UPDATE User_Teacher SET User_Teacher.teacher_special = 'notdefault'
+  where User_Teacher.user_id = '$User_ID' ";
+  $response = mysqli_query($conn, $select);
 
 
+} else if ($answer == 2) {
+  $answer_result   = '일반강사/Community_Tutor';
+  $select = "UPDATE User_Teacher SET User_Teacher.teacher_special = 'default'
+  where User_Teacher.user_id = '$User_ID' ";
+  $response = mysqli_query($conn, $select);
 
 
+} else if ($answer == 3) {
+  $answer_result   = '거절/Denined';
 
-// 프론트단에서 전달받은 시간별 칸 값을 _ 기호를 기준으로 분리한다. 
-$explan = (explode("_", $plan));
-$tzplan = array(); //Timezone 적용을 위한 배열 생성
-foreach($explan as $val){ 
-$save = $val - $timezone* $hour;
-$date = date('Y-m-d H:i:s',  $save);
+  $select = "UPDATE User_Detail SET User_Detail.teacher_register_check = 'default'
+  where User_Detail.user_id = '$User_ID' ";
+  $response = mysqli_query($conn, $select);
 
-
-
-//1시간 = 3600000;
-$hour   = 3600000;
-
-// 프론트단에서 전달받은 시간별 칸 값을 _ 기호를 기준으로 분리한다. 
-$exschedule = (explode("_", $schedule));
-// echo $exschedule;
-
-$tzschedule = array(); //Timezone 적용을 위한 배열 생성
-foreach($exschedule as $val){ 
-$save = $val - $timezone* $hour;
-// $date = date('Y-m-d H:i:s',  $save);
-
-// echo $save;
-
-// Class_Add DB TABLE에 저장 
-$sqlClassAdd = "INSERT INTO Class_Add (user_id_student, user_id_teacher, class_id, class_time, schedule_list, class_register_memo, class_register_status , class_register_review , class_register_review_student ,class_register_method, class_register_answer_date, class_register_date) 
-           VALUES ('$User_ID', '$tusid','$class_id', '$tp', '$save', '$class_register_memo', '0','0','0', '$class_method', '0' , now())";
-// $sqlClassAdd = "INSERT INTO Class_Add (user_id_student, user_id_teacher, class_id, class_time, schedule_list, class_register_memo, class_register_status ,class_register_method, class_register_answer_date, class_register_date) 
-//            VALUES ('1', '2', '3', '4', '5555555555','6', '0', '33', '0' , now())";
-
-$insert = mysqli_query($conn, $sqlClassAdd);
-
-if ($sqlClassAdd) 
-$last_CAID = mysqli_insert_id($conn); // 마지막으로 insert 된 값의 idx 값 가져오기 용도: 메일 전송후 수업 예약 승낙 여부를 Class_ADD tb내 C_A_Status 상태 변경용. 
-// echo 'ff'. $last_CAID ;
-
-
-
-if ($insert) { //정상일떄
-
-  $select = "UPDATE Teacher_Schedule  SET teacher_schedule_status = '0' where user_id_teacher = '$tusid' and schedule_list = '$save'";
-
-$response4 = mysqli_query($conn, $select);
-
-
-if ($response4) { //정상일떄
-  
-  
-  $data = array(
-    'schedule_list'            =>   'Teacher_Schedule 값 업뎃 됨',
-    'success'        	=>	'yes'   
-  );
-  // echo json_encode($data);
-  // mysqli_close($conn);
-} else {//비정상일떄 
-  $data = array(
-    'schedule_list'            =>   'Teacher_Schedule 값 업뎃 안됨',
-    'success'        	=>	'no'
-  );
-  // echo json_encode($data);
-  // mysqli_close($conn);
-}
-  
-  $data = array(
-    'schedule_list'            =>   'class_add  잘됨',
-    'success'        	=>	'yes',
-    'lastclassregisterid'        	=>	$last_CAID
-  );
-  // echo json_encode($data);
-  // mysqli_close($conn);
-} else {//비정상일떄 
-  $data = array(
- 
-    'success'        	=>	'no'
-  );
-  // echo json_encode($data);
-  // mysqli_close($conn);
 }
 
 
 
-array_push($tzplan,$save);
-}
-$tzplanresult = implode("_",$tzplan); // 다시 합체 
 
 
 
 
-array_push($tzschedule,$save);
-}
-$tzscheduleresult = implode("_",$tzschedule); // 다시 합체 
-
-
-
-
-// 위의 수업을 개설한 강사의 '이름','email', 'timezone' 정보 
-$sql = "SELECT User.user_name, User.user_email, User_Detail.user_timezone
-FROM User
-JOIN User_Detail ON User.user_id = User_Detail.user_id
- where User.user_id = '$tusid' ";
-$result = mysqli_query($conn, $sql);
-$row1 = mysqli_fetch_array($result);
-
- $tname = $row1['0'];
- $temail = $row1['1'];
- $tTimezone = $row1['2'];
-
-
-
-
-// 신청한 수업 일정(schedule) 값을 강사에게 보내주기 위한 코드 이며 강사의 TIMEZONE 기준으로 변환 되어 $sendtimeresult 에 저장됨 . 
-$sendtime = array();
-$i =0;
-foreach($exschedule as $val){
-
- $save1 = ($val + $tTimezone * $hour)/1000 ;
-// echo $save2 = $save1/1000;
-
- $date = date('Y-m-d H:i:s', $save1);
- $date = date('Y-m-d H:i:s', $save1);
-
-$i =$i +1;
- $i;
- $i;
-array_push($sendtime,$date);
-}
-$sendtimeresult = implode(",  ",$sendtime);
 
 
 //메일 전송 
-$subject = 'Hangle Square : '.$U_Name.' want register your class';
+$subject = 'Hangle Square : The answer to the tutor registration application has arrived. / 튜터 등록 신청에 대한 답변이 도착하였습니다. ';
 $message = '
-'.$U_Name.' want to make a reservation your class!'. "<br/>".'
-Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.'. "<br/>".'
+Thank you for registering as an instructor for our service.' . ' "<br/>".
+
+Answer/답변: . "<br/>".
+저희 서비스에 강사 신청을 해주셔서 감사합니다.' . ' "<br/>".
+Thank you for registering as an instructor for our service.' . '"<br/>"' . ' "<br/>".
+
+' . $answer_text . "<br/>" . '
+' . $U_Name . ' has been registered as a ' . $answer_result  . '. / ' . $U_Name . ' 님은  '. $answer_result .' 으로 (등록) 되었습니다.  ' . "<br/>" . '
  
-------------------------'. "<br/>".'
-Student-Email: '.$U_Email.''. "<br/>".'  
-Student-Name: '.$U_Name.''. "<br/>".'
-Class-Name: '.$clname.''. "<br/>".'
-Class-Type: '.$cltype.''. "<br/>".'
-Class-Level: '.$cllevel.''. "<br/>".'
-Class-TIME: '.$tp.' min'. "<br/>".'
-Schedule:  '.$i.'time(s)'. "<br/>".''.$sendtimeresult.''. "<br/>".'
-Message: '.$class_register_memo."<br/>".'
-------------------------'. "<br/>".'
- 
+';
 
 
-'; // Our message above including the link;
-try{
-$mail = new PHPMailer(true);
-$mail->CharSet = "utf-8";   //한글이 안깨지게 CharSet 설정
-$mail->Encoding = "base64";
-$mail->isSMTP();
-$mail->SMTPAuth = true;
 
-$mail->Host = "smtp.gmail.com";
-$mail->Username = "apswtrare@gmail.com";
-$mail->Password = "caofvauzywfzrbxv";
-$mail->Password = "caofvauzywfzrbxv";
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
-$mail->setFrom('apswtrare@gmail.com');
-$mail->addAddress($temail);
-$mail->isHTML(true);
-$mail->Subject = $subject;
-$mail->Body = $message;
+try {
+  $mail = new PHPMailer(true);
+  $mail->CharSet = "utf-8";   //한글이 안깨지게 CharSet 설정
+  $mail->Encoding = "base64";
+  $mail->isSMTP();
+  $mail->SMTPAuth = true;
 
-$mail->send();
+  $mail->Host = "smtp.gmail.com";
+  $mail->Username = "apswtrare@gmail.com";
+  $mail->Password = "caofvauzywfzrbxv";
+  $mail->Password = "caofvauzywfzrbxv";
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
+  $mail->setFrom('apswtrare@gmail.com');
+  $mail->addAddress($temail);
+  $mail->isHTML(true);
+  $mail->Subject = $subject;
+  $mail->Body = $message;
+
+  $mail->send();
 
 
-$data = array(
- 
-  'success'           => "yes",
-  'class_register_id'        	=>	$last_CAID
-);
-echo json_encode($data);
+  $data = array(
+
+    'success'           => "yes",
+    'message'           => "등록신청 답변 완료",
+  
+  );
+  echo json_encode($data);
   mysqli_close($conn);
-
 } catch (Exception $e) {
-//비정상일떄 
-$data = array(
- 
-  'success'           => "Message could not be sent . Mailer Error: {
-    $mail->ErrorInfo}"
-);
-echo json_encode($data);
-mysqli_close($conn);
+  //비정상일떄 
+  $data = array(
 
- ;
+    'success'           => "Message could not be sent . Mailer Error: {
+    $mail->ErrorInfo}"
+  );
+  echo json_encode($data);
+  mysqli_close($conn);;
 }
