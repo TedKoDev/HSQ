d8a44b9a821dec7050f91ca76a1097a39771a030
ahnhaein <ahnhaein626@gmail.com>
2023-01-02 14:44:16 +0900

    json 변경 함수중 가격 관련된 함수 추가해야함

--- findclass/classfilter.js
+++ findclass/classfilter.js
@@ -8,7 +8,7 @@ export async function classfilter() {
     const user_timezone = await getMyUtc(getCookie(cookieName));
     request_to_server.user_timezone = user_timezone;
     
-
+    
     // 선택한 필터가 표시되는 div
     const filterItem_div = $('.filterItemList');
 
@@ -67,7 +67,7 @@ export async function classfilter() {
                     
                 const new_btn = document.createElement('div');
                 new_btn.innerHTML = `
-                    <div id = "${filterValue}_div" value = ${filterValue} class = "flex items-center text-xs px-1 py-2 text-center  bg-gray-300 hover:bg-gray-400 rounded-2xl mx-1">
+                    <div id = "${filterValue}_div" value = ${filterValue} class = "flex items-center text-xs px-1 py-2 text-center bg-gray-300 hover:bg-gray-400 rounded-2xl mx-1">
                         <span class = "mr-1">${text}</span>
                         <button value = "${filterValue}" class="text-gray-800 font-medium inline-flex items-center" type="button">
                             <svg name = "deleteIcon" class= "deleteIcon w-4 h-4 bg-gray-500 rounded-full text-white border-white" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>
@@ -146,6 +146,12 @@ export async function classfilter() {
                           
             const delete_div = document.getElementById(target.value+"_div");
             delete_div.remove();
+
+            // 만약 삭제한게 search일 경우 검색창 값 초기화
+            if (target.id == 'search_item_btn') {
+
+                search_input.value = "";
+            }
         } 
            
     })
@@ -198,12 +204,114 @@ export async function classfilter() {
                 
             }
         }       
-    }           
+    }     
+    
+    function clickInputBtn(search_input) {
+
+        // 필터 아이템 리스트에서 해당 필터 종류 모두 초기화
+        const search_div_list = $_all('.search_item');    
+        for (const search_item of search_div_list) {
+
+            search_item.remove();
+        } 
+
+        const filterValue = search_input.value;
+
+        const new_btn = document.createElement('div');
+        new_btn.innerHTML = `
+            <div id = "${filterValue}_div" value = ${filterValue} class = "search_item flex items-center text-xs px-1 py-2 text-center  bg-gray-300 hover:bg-gray-400 rounded-2xl mx-1">
+                <span class = "mr-1">${filterValue}</span>
+                <button value = "${filterValue}" id = "search_item_btn" class="text-gray-800 font-medium inline-flex items-center" type="button">
+                    <svg name = "deleteIcon" class= "deleteIcon w-4 h-4 bg-gray-500 rounded-full text-white border-white" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>
+                </button>                
+            </div>
+            `;              
+                            
+        filterItem_div.append(new_btn);
+    }
+    // 검색 버튼 클릭할 때 이벤트
+    search_btn.addEventListener('click', () => {
+
+        clickInputBtn(search_input);
+    })
 }
 
 // 이벤트에 따라 서버에 보낼 json 변경
-export function changeJson() {
+export function changeJson(key, value, type) {
 
+    // type : add / update / delete (추가, 변경, 삭제인지 판별)
+    // var / array : var이면 변경/삭제, array면 추가/삭제
+
+    if (key == 'filter_search') {
+        
+        checkType(key, value, type, 'var');     
+    }
+    else if (key == 'filter_class_price') {
+
+    }
+    else if (key == 'filter_teacher_special') {
+        
+        checkType(key, value, type, 'var');
+    }
+    else if (key == 'filter_date') {
+        
+        checkType(key, value, type, 'var');
+    }
+    else if (key == 'filter_time') {
+        
+        checkType(key, value, type, 'array');
+    }
+    else if (key == 'filter_class_type') {
+        
+        checkType(key, value, type, 'array');
+    }
+    else if (key == 'filter_teacher_sex') {
+        
+        checkType(key, value, type, 'var');
+    }
+    else if (key == 'filter_teacher_country') {
+        
+        checkType(key, value, type, 'array');
+    }
+    else if (key == 'filter_teacher_language') {
+        
+        checkType(key, value, type, 'array');
+    }
+
+    function checkType(key, value, type, var_array) {
+       
+        const check = var_array;
+
+        if (type == 'add') {            
+
+            request_to_server[key].push(value);
+        }
+        else if (type == 'update') {
+
+            request_to_server[key] = value;
+        }
+        else if (type == 'delete') {
+
+            if (check = 'var') {
+
+                request_to_server[key] = '';
+            }
+            else if (check = 'array') {
+
+                for(let i = 0; i < request_to_server[key].length; i++) {
+                    if(request_to_server[key][i] === value)  {
+                        request_to_server[key].splice(i, 1);
+                      i--;
+                    }
+                }
+            }            
+        }
+    }
+
+    // 수강료 관련 필터일 경우 (최소/최고 가격 때문에 별도의 함수로 처리)
+    function checkClassPrice(value, type) {
+        
+    }
 }
 
 // let test = {
