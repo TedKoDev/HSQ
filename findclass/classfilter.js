06022fc390cb19b4111033c62e9c388d979ba204
ahnhaein <ahnhaein626@gmail.com>
2023-01-02 16:32:27 +0900

    json 변경 함수 및 적용 완료

--- findclass/classfilter.js
+++ findclass/classfilter.js
@@ -220,7 +220,7 @@ export async function classfilter() {
         }       
     }     
     
-    function clickInputBtn(search_input) {
+    function clickInputBtn(search_input, e) {
 
         // 필터 아이템 리스트에서 해당 필터 종류 모두 초기화
         const search_div_list = $_all('.search_item');    
@@ -235,18 +235,21 @@ export async function classfilter() {
         new_btn.innerHTML = `
             <div id = "${filterValue}_div" value = ${filterValue} class = "search_item flex items-center text-xs px-1 py-2 text-center  bg-gray-300 hover:bg-gray-400 rounded-2xl mx-1">
                 <span class = "mr-1">${filterValue}</span>
-                <button value = "${filterValue}" id = "search_item_btn" class="text-gray-800 font-medium inline-flex items-center" type="button">
+                <button value = "${filterValue}" name = "filter_search" id = "search_item_btn" class="text-gray-800 font-medium inline-flex items-center" type="button">
                     <svg name = "deleteIcon" class= "deleteIcon w-4 h-4 bg-gray-500 rounded-full text-white border-white" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>
                 </button>                
             </div>
             `;              
                             
         filterItem_div.append(new_btn);
+
+        // json 값 변경        
+        changeJson("filter_search", filterValue, 'update');
     }
     // 검색 버튼 클릭할 때 이벤트
-    search_btn.addEventListener('click', () => {
+    search_btn.addEventListener('click', (e) => {
 
-        clickInputBtn(search_input);
+        clickInputBtn(search_input, e);
     })
 }
 
