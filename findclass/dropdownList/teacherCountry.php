e733d5f2c6caaa17ee9ab3c4d269e80137bb5709
TedKoDev <ted.kordev@gmail.com>
2023-01-11 07:32:59 +0000

    23-01-11 main 넣는중

--- findclass/dropdownList/teacherCountry.php
+++
@@ -1,9 +0,0 @@
-<div id="teacherCountry" class="hidden z-10 w-60 bg-white rounded-xl shadow">
-    <div class = "teacherCountryList my-2 justify-center px-1">  
-    <?php foreach ($countryList as $country) {        
-        ?><input type="checkbox" id = "<?php echo $country; ?>" name = "filter_teacher_country" value="<?php echo $country; ?>" class="hidden filter_checkbox" />
-        <label id = "<?php echo $country; ?>_l" for="<?php echo $country; ?>" class="text-gray-800 bg-gray-300 hover:bg-gray-400 font-medium rounded-2xl text-xs px-2 mx-1 py-2 my-1 text-center inline-flex items-center"
-        ><?php echo $country; ?></label>
-    <?php } ?>      
-    </div>           
-</div>
